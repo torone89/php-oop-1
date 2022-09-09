47f4a1941d47f32b3d0cea028633c4ca3f11e794
torone89 <torone89>
2022-09-09 13:52:44 +0200

    Adding Construct

--- includes/class/data.php
+++ includes/class/data.php
@@ -11,4 +11,17 @@ class films
     public string $genre;
     public string $year;
     public string $poster_path;
+
+// CONSTRUCT 
+    function __construct($_title, $_original_title, $_overview, $_original_language, $_genre, $_year, $_poster_path)
+    {
+        $this->title = $_title;
+        $this->original_title = $_original_title;
+        $this->genre = $_genre;
+        $this->year = $_year;
+        $this->overview = $_overview;
+        $this->original_language = $_original_language;
+        $this->_poster_path = $_poster_path;
+    }
+
 }
